7f56383af0a9df8ede3fd7a8d07e15b205decc1f
bogdan22tsb <bodan22tsb@gmail.com>
2024-10-05 11:14:10 +0300

    testing

--- src/home.js
+++ src/home.js
@@ -13,7 +13,7 @@ wheat1.setAttribute('class', 'wheat1');
 h1Title.setAttribute('class', 'h1Title');
 wheat2.setAttribute('class', 'wheat2');
 
-h1Title.textContent = 'Restaurant name';
+h1Title.textContent = 'Restaurant name lol';
 
 title.append(wheat1);
 title.append(h1Title);
